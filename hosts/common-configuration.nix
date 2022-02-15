a3244a10df87610cebf1796b1977f0cda9e9dd36
Neru Ninja <mail@neru.ninja>
2022-02-15 07:39:37 +0000

    Enforce declarative user management
    Enabling the `users.mutableUsers` will ensure that the only users that
    exist on the system are the users defined in the NixOS configuration.
    This disables the imperative usage of the `useradd` and `groupadd`
    commands, and the contents of `/etc/passwd` and `/etc/group` will be
    synced on system activation.

--- hosts/common-configuration.nix
+++ hosts/common-configuration.nix
@@ -1,12 +1,34 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  passwordFileDirectory = "/etc/nixos/users/passwords";
+  neruHashedPasswordFile = "${passwordFileDirectory}/neru";
+  rootHashedPasswordFile = "${passwordFileDirectory}/root";
+in
 {
-  users.users = {
-    neru = {
-      description = "Neru Ninja";
-      home = "/home/neru";
-      extraGroups = [ "wheel" ];
-      isNormalUser = true;
-    };
+  imports = [ ../secrets ];
+
+  users.mutableUsers = false;
+
+  users.users.neru = {
+    isNormalUser = true;
+    description = "Neru Ninja";
+    home = "/home/neru";
+    extraGroups = [ "wheel" ];
+    passwordFile = neruHashedPasswordFile;
+  };
+
+  secrets.neru-password = {
+    encryptedFile = ../secrets/user-passwords/neru.asc;
+    decryptionPath = neruHashedPasswordFile;
+  };
+
+  users.users.root = {
+    passwordFile = rootHashedPasswordFile;
+  };
+
+  secrets.root-password = {
+    encryptedFile = ../secrets/user-passwords/root.asc;
+    decryptionPath = rootHashedPasswordFile;
   };
 }
